33144d64a84c065a0ab721bdad1077d3d99048f8
AndeAndreas31415 <andreas_coding@gmx.de>
2020-10-31 22:39:52 +0100

    add eggs move

--- base.js
+++
@@ -1,8 +0,0 @@
-function brain(){
-	document.location = "main.html"
-}
-
-function eggover(){
-	 alert("Test");
-/*	alert(document.getElementById("egg").visibilityState);*/
-}
---
+++ script/base.js
@@ -0,0 +1,38 @@
+function brain(){
+	document.location = "main.html"
+}
+
+function github(){
+	document.getElementById("gh").style.visibility = "visible";
+	document.getElementById("gh").animate([
+		{left: "50%"},
+		{left: "0%"},
+		{left: "10%"},
+		{left: "5%"}
+	],{
+		duration: 5000,
+	});
+}
+
+function repo(){
+	document.getElementById("gp").style.visibility = "visible";
+	document.getElementById("gp").animate([
+		{left: "50%"},
+		{left: "5%"},
+		{left: "15%"},
+		{left: "5%"}
+	],{
+		duration: 5000,
+	});
+}
+
+function eggover(){
+	document.getElementById("egg").style.visibility = "visible";
+	github();
+	repo();
+}
+
+function egg(){
+	document.getElementById("egg").style.visibility = "visible";
+	console.log("please don't smash eggs");
+}
