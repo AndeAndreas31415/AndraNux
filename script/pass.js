ca5eecf1ada93f430c0ec06879e280527b02cc37
AndeAndreas31415 <andreas_coding@gmx.de>
2022-05-28 15:10:41 +0200

    ctf lock screen extract to a separate site

--- script/pass.js
+++ script/pass.js
@@ -1,20 +1,45 @@
 var x = window.location.pathname;
-var xa = x.split("/")
+var xa = x.split("/");
+var existctf = false;
 
 if (document.cookie == "") {
     console.log("Is this a CTF-Challange?");
     console.log("Show in the script/pass.js <- login()");
+    if (xa[xa.length-1] != "lock.html") {
+        aC("site",x,1/48);
+    }
     npass();
 } else {
     var decodedCookie = decodeURIComponent(document.cookie);
     var cookie = decodedCookie.split('; ');
+    if (xa[xa.length-1] != "lock.html") {
+        for (let cil = 0; cil < cookie.length; cil++) {
+            if (cookie[cil].startsWith('site')) {
+                removeCookie('site');
+            }
+        }
+        aC("site",x,1/48);
+    }
+    ctfcock();
+}
+
+function sitecock(){
+    for (let cil = 0; cil < cookie.length; cil++) {
+        if (cookie[cil].startsWith('site')) {
+            var ca = cookie[cil].split('=');
+            document.location = ca[1];
+        }
+    }
+}
+
+function ctfcock(){
     for (let cil = 0; cil < cookie.length; cil++) {
         if (cookie[cil].startsWith('ctf')) {
+            existctf = true
             var ca = cookie[cil].split('=');
             if (ca[1] == "2049965476ab49cd5136578869ef-d0986fe9bb9eb0eb1") {
-                if (xa[xa.length-1] == "inctf.html") {
-                    document.getElementById("ipasswdiv").style.visibility = "hidden";
-                    document.getElementById("pages").style.visibility = "visible";
+                if (xa[xa.length-1] == "lock.html") {
+                    sitecock();
                 }
             } else if (ca[1] == "") {
                 npass();
@@ -25,11 +50,14 @@ if (document.cookie == "") {
                 npass();
             }
         }
+        if(cil == cookie.length-1 && !existctf) {
+            npass();
+        }
     }
 }
 
 function npass() {
-    if (xa[xa.length-1] != "inctf.html") {
+    if (xa[xa.length-1] != "lock.html") {
         for (let diri = 0; diri < xa.length; diri++) {
             if (xa[diri] == "ctf") {
                 diffdir = xa.length-diri-2;
@@ -39,11 +67,8 @@ function npass() {
         for (let ddi = 0; ddi < diffdir; ddi++) {
             dir += "../";
         }
-        dir += "inctf.html";
+        dir += "lock.html";
         document.location = dir;
-    } else {
-        document.getElementById("ipasswdiv").style.visibility = "visible";
-        document.getElementById("pages").style.visibility = "hidden";
     }
     const input = document.querySelector('input');
     input.addEventListener('input', login);
@@ -88,7 +113,6 @@ function brutfuc(cookie) {
                 return;
             }
         } else {
-            console.log("Fuck");
             aC("brutfa","1",(1/72));
             return;
         }
@@ -99,7 +123,7 @@ function aC(nc,scoo,ld) {
     var d = new Date();
     d.setTime(d.getTime() + (ld*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
-    document.cookie = nc+"="+scoo+"; "+expires+"; secure";
+    document.cookie = nc+"="+scoo+"; "+expires+"; path=/; secure";
 }
 
 function removeCookie(sr) {
